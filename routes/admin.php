fab877e595e4cb3cd63e1a0be6a272b7401fb71a
Sina <sharrpas@gmail.com>
2023-03-28 12:01:56 +0330

    fix: personal access token expiration is 2houre

--- routes/admin.php
+++ routes/admin.php
@@ -12,5 +12,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('gyms',[GymController::class,'index'])->middleware('role:super_admin');
-Route::post('add-gym',[GymController::class,'add_gym'])->middleware('role:super_admin');
+Route::middleware(['auth:sanctum', 'role:super_admin'])->group(function () {
+
+Route::get('gyms',[GymController::class,'index']);
+Route::post('add-gym',[GymController::class,'add_gym']);
+
+});
