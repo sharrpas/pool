1da281f14c936c4b0d7cf2224e8c278965e9616c
Sina <sharrpas@gmail.com>
2023-04-03 17:33:48 +0330

    fix: super admin bug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,7 +39,7 @@ class UserController extends Controller
         if ($user && $pass_check) {
             return $this->success([
                 'user' => $user->name,
-                'gym_name' => $user->gym()->first()->name,
+                'gym_name' => $user->gym()->first()->name ?? null,
                 'username' => $user->username,
                 'role' => $user->roles()->first()->name,
                 'token' => $user->createToken('token_base_name')->plainTextToken
