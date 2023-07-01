eaf32e075e1ef26539e6e7c6817758c1eb8b2349
Sina <sharrpas@gmail.com>
2023-07-01 11:42:34 +0330

    fix: buffet in dashboard

--- app/Http/Resources/TableResource.php
+++ app/Http/Resources/TableResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Buffet;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Carbon;
 
@@ -21,7 +22,23 @@ class TableResource extends JsonResource
             $now = Carbon::now();
             $play_time = Carbon::parse($task->opened_at)->diffInMinutes($now);
             $play_price = round(((int)(($this->price) / 60 * $play_time)), -2);
+
+            if ($task->buffet != null) {
+                $task_buffets = array_map('intval', explode(',', $task->buffet));
+                sort($task_buffets);
+
+                $buffets = Buffet::query()->whereIn('id', $task_buffets)->get();
+                $ids = $buffets->pluck('id')->toArray();
+                $buffetsInArray = BuffetResource::collection($buffets);
+                $finalBuffets = collect($task_buffets)->map(function ($id) use ($ids, $buffetsInArray) {
+                    return $buffetsInArray[array_search($id, array_values($ids))];
+                });
+            }
         }
+
+
+
+
         return [
             'id' => $this->id,
             'gym_id' => $this->gym_id,
@@ -33,6 +50,8 @@ class TableResource extends JsonResource
             'price_so_far' => $play_price ?? null,
             'duration' => $play_time ?? null,
             'opened_at' => $task ? substr($task->opened_at, 11, 5) : null ,
+
+            'buffet' => $finalBuffets ?? null,
 
 
         ];
