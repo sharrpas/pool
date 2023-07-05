fef491eb7b6b11a1780b28b305032103fcee5526
Sina <sharrpas@gmail.com>
2023-07-05 19:36:27 +0330

    fix: gymresource in user prudacts

--- app/Http/Resources/User/ProductResource.php
+++ app/Http/Resources/User/ProductResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\User;
 
+use App\Http\Resources\GymResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
 
@@ -26,7 +27,7 @@ class ProductResource extends JsonResource
                 return Storage::url('images/stores/' . $image);
             }, $images),
             'mobile' => $this->whenLoaded('gym')->manager->mobile,
-            'gym' => $this->whenLoaded('gym'),
+            'gym' => GymResource::make($this->whenLoaded('gym')),
         ];
     }
 }
