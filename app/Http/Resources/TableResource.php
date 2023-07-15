74fc8719dc1a10b221c1e3d196a3ea9be38dead4
Sina <sharrpas@gmail.com>
2023-07-15 22:29:51 +0330

    fix: return buffet price in dashboard

--- app/Http/Resources/TableResource.php
+++ app/Http/Resources/TableResource.php
@@ -49,6 +49,7 @@ class TableResource extends JsonResource
             'player' => $playerr? UserResource::make($playerr) : null,
 
             'buffet' => $finalBuffets ?? null,
+            'buffet_price' => $task->buffet_price ?? null,
 
 
         ];
